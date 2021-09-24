b725a1cd2a29a4c77f8ca9eaf1d637ed49fb2dd9
Mantas <mantaskrincius@gmail.com>
2021-09-24 20:11:36 +0300

    done for now :)

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Search from "./Components/Search";
 import Dropdown from "./Components/Dropdown";
+import {useState} from "react";
+import Translate from "./Components/Translate";
+import Accordion from "./Components/Accordion";
+import Route from "./Components/Route";
+import Header from "./Components/Header";
+
 // import Accordion from "./Components/Accordion";
 
 function App() {
+
+
     const items = [
         {
             title: "what is react?",
@@ -17,11 +25,39 @@ function App() {
             content: "React redux"
         }
     ]
+
+    const options = [
+        {
+            label: "Color RED",
+            value: "red"
+        },
+        {
+            label: "Color GREEN",
+            value: "green"
+        },
+        {
+            label: "Color BLUE",
+            value: "blue"
+        }
+    ]
+    
+
+    const [selected, setSelected] = useState(options[0])
     return (
         <div className="App">
-            {/*<Accordion items={items}/>*/}
-            {/*<Search/>*/}
-            <Dropdown/>
+            <Header/>
+            <Route path="/">
+                <Accordion items={items}/>
+            </Route>
+            <Route path="/list">
+                <Search/>
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown label="Select Color" options={options} onSelectedChange={setSelected} selected={selected}/>
+            </Route>
+            <Route path="/translate">
+                <Translate/>
+            </Route>
         </div>
 
     );
--- widgets/src/Components/Dropdown.js
+++ widgets/src/Components/Dropdown.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 
-const Dropdown = () => {
+const Dropdown = ({options, selected, onSelectedChange, label}) => {
+    const [open, setOpen] = useState(false)
+    const ref = useRef()
 
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+        document.body.addEventListener("click", onBodyClick, { capture: true });
+
+        return () => {
+            document.body.removeEventListener("click", onBodyClick, {
+                capture: true,
+            });
+        };
+    }, []);
+
+        const renderOption = options.map((option) => {
+        if (option.value === selected.value) {
+            return null
+        }
+        return (
+            <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
+                {option.label}
+            </div>
+        )
+    })
     return (
-        <div>
-<h1>DropDown</h1>
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">{label}</label>
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+                    <i className="dropdwon icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
+                        {renderOption}
+                    </div>
+                </div>
+            </div>
+            {/*<h1 style={{color: `${selected.value}`}}>Hello</h1>*/}
         </div>
     )
 }
